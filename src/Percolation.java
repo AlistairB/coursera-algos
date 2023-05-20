1c2c0bd984b0318a6cc337226560775a5e039def
Alistair Burrowes <alistair.burrowes@gmail.com>
2023-05-21 09:56:42 +1000

    Working throuch percolation

--- src/Percolation.java
+++ src/Percolation.java
@@ -6,14 +6,31 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    private WeightedQuickUnionUF wqu;
+    private boolean[][] grid;
+    private int maxIndex;
+
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
+        if (n <= 0)
+            throw new IllegalArgumentException();
 
+        wqu = new WeightedQuickUnionUF(n);
+        grid = new boolean[n][n];
+        maxIndex = n - 1;
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
+        grid[row][col] = true;
 
+
+    }
+
+    private void connectIfOpenAndExists(int fromRow, int fromCol, int toRow, int toCol) {
+        if (toRow >= 0 && toRow <= maxIndex && toCol >= 0 && toCol <= maxIndex) {
+
+        }
     }
 
     // is the site (row, col) open?
