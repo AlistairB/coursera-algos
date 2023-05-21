dbceb13a7524bd58b3e7766f926cae363ed41f44
Alistair Burrowes <alistair.burrowes@gmail.com>
2023-05-21 19:34:33 +1000

    initial perc done

--- src/Percolation.java
+++ src/Percolation.java
@@ -9,6 +9,9 @@ public class Percolation {
     private WeightedQuickUnionUF wqu;
     private boolean[][] grid;
     private int maxIndex;
+    private int size;
+
+    private int openSites;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
@@ -17,39 +20,61 @@ public class Percolation {
 
         wqu = new WeightedQuickUnionUF(n);
         grid = new boolean[n][n];
+        size = n;
         maxIndex = n - 1;
+        openSites = 0;
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        grid[row][col] = true;
+        grid[row - 1][col - 1] = true;
+        int value = row * col;
 
+        connectIfOpenAndExists(value, row + 1, col);
+        connectIfOpenAndExists(value, row - 1, col);
+        connectIfOpenAndExists(value, row, col + 1);
+        connectIfOpenAndExists(value, row, col - 1);
 
+        openSites++;
     }
 
-    private void connectIfOpenAndExists(int fromRow, int fromCol, int toRow, int toCol) {
-        if (toRow >= 0 && toRow <= maxIndex && toCol >= 0 && toCol <= maxIndex) {
+    private boolean gridVal(int row, int col) {
+        return grid[row - 1][col - 1];
+    }
 
+    private void connectIfOpenAndExists(int source, int row, int col) {
+        if (row >= 0 && row <= maxIndex && col >= 0 && col <= maxIndex && gridVal(row, col)) {
+            wqu.union(source, row * col);
         }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return false;
+        return gridVal(row, col);
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
+        int value = row * col;
+
+        for (int i = 1; i <= size; i++) {
+            if (wqu.connected(value, i)) return true;
+        }
+
         return false;
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        return -1;
+        return openSites;
     }
 
     // does the system percolate?
     public boolean percolates() {
+        for (int i = 1; i <= size; i++) {
+            if (isFull(size, i)) return true;
+        }
+
         return false;
     }
 
