600c4d3ffa0706061632ebc9d4b0e6601c417253
Alistair Burrowes <alistair.burrowes@gmail.com>
2023-06-11 13:11:16 +1000

    Makefile create zip

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 
 
+percolation:
+	./script/build.sh percolation
+
--- script/build.sh
+++ script/build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -Eexuo pipefail
 
-# copy files into tmp dir
+cd "$(dirname "$0")"/.. || exit
 
-# remove the package statements
+declare module_name="$1"
+
+# copy files into tmp dir
+mkdir -p "./tmp/$module_name"
+cp -a "./src/main/java/$module_name/" "./tmp/"
+
+# remove the first line to remove the pacakge statements
+find ./tmp -type f -exec sed -i '1d' "{}" \;
 
 # zip
+rm "./tmp/$module_name.zip"
+zip -j -r "./tmp/$module_name.zip" "./tmp/$module_name/"
