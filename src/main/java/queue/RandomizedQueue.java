e447c4e449288e800853d1030c984ede5c7608ed
Alistair Burrowes <alistair.burrowes@gmail.com>
2023-07-05 15:55:12 +1000

    WEEP - Working on randomized queue

--- src/main/java/queue/RandomizedQueue.java
+++ src/main/java/queue/RandomizedQueue.java
@@ -1,42 +1,119 @@
 package queue;
 
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Iterator;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-    // construct an empty randomized queue
-    public RandomizedQueue() {
+    private Item[] queue;
+    private int capacity;
 
+    // construct an empty randomized queue
+    @SuppressWarnings("unchecked")
+    public RandomizedQueue() {
+        capacity = 2;
+        queue = (Item[]) new Object[capacity];
     }
 
     // is the randomized queue empty?
     public boolean isEmpty() {
-        return false;
+        return size() > 0;
     }
 
     // return the number of items on the randomized queue
     public int size() {
-        return 0;
+        return queue.length;
     }
 
     // add the item
     public void enqueue(Item item) {
+        if (size() == capacity) {
+            scaleUpCapacity();
+        }
 
+        queue[size()] = item;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void scaleUpCapacity() {
+        var newCapacity = capacity * 2;
+        var newQueue = (Item[]) new Object[newCapacity];
+
+        for (var i = 0; i < queue.length; i++) {
+            newQueue[i] = queue[i];
+        }
+
+        queue = newQueue;
+        capacity = newCapacity;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void scaleDownCapacity() {
+        var newCapacity = capacity / 2;
+        var newQueue = (Item[]) new Object[newCapacity];
+
+        for (var i = 0; i < queue.length; i++) {
+            newQueue[i] = queue[i];
+        }
+
+        queue = newQueue;
+        capacity = newCapacity;
     }
 
     // remove and return a random item
     public Item dequeue() {
-        return null;
+        var toRemoveIndex = StdRandom.uniformInt(0, size() - 1);
+
+        var removed = queue[toRemoveIndex];
+        queue[toRemoveIndex] = null;
+
+        if (size() < (capacity / 4)) {
+            scaleDownCapacity();
+        }
+
+        return removed;
     }
 
     // return a random item (but do not remove it)
     public Item sample() {
-        return null;
+        var toGetIndex = StdRandom.uniformInt(0, size() - 1);
+
+        return queue[toGetIndex];
     }
 
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
         return null;
+    }
+
+    private class RandomizedQueueIterator implements Iterator<Item> {
+        Deque.Node current;
+
+        // initialize pointer to head of the list for iteration
+        public DequeIterator(Deque<Item> deque)
+        {
+            current = deque.first;
+        }
+
+        // returns false if next element does not exist
+        public boolean hasNext()
+        {
+            return current != null && current.next != null;
+        }
+
+        // return current data and update pointer
+        public Item next()
+        {
+            current = current.next;
+            return current.value;
+        }
+
+        // implement if needed
+        public void remove()
+        {
+            throw new UnsupportedOperationException();
+        }
     }
 
     // unit testing (required)
